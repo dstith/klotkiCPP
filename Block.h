385f73014c67807603b666430198da1861629066
dstith <dstith@outlook.com>
2015-05-10 11:21:16 -0700

    Update Block.h

--- Block.h
+++ Block.h
@@ -11,16 +11,19 @@ using namespace std;
 class Block {
 	private:
 		char fillChar;
+		char name;
 		char** block;
 		int rowSize;
 		int colSize;
 	public:
 		Block();
-		Block(char);
-		Block(char, int, int);
+		Block(char, char);
+		Block(char, char, int, int);
 		~Block();
 		void setFill(char);
+		void setName(char);
 		char getFill();
+		char getName();
 		void fillBlock();
 		void printBlock();
 		char** getArray();
